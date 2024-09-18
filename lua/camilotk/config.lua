365351ae253888875907a2feda562bdbacce020a
Camilo Cunha de Azevedo <camilotk@gmail.com>
2024-09-18 09:21:43 -0300

    add a conf to display the highlight over the yanked lines

--- lua/camilotk/config.lua
+++ lua/camilotk/config.lua
@@ -1,4 +1,4 @@
--- config
+---- CONFIG
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
@@ -11,3 +11,19 @@ vim.opt.smartcase = true
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+
+---- DISPLAY HIGHLIGHT ON YANK
+-- Create an augroup to organize the autocommand
+vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+
+-- Add an autocommand that triggers on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = 'YankHighlight',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',  -- You can change this to any highlight group
+      timeout = 400,          -- Duration of the highlight in milliseconds
+    })
+  end,
+})
+
